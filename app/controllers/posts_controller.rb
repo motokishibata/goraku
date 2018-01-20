370116a6de1177a88eeaf2622d4c9c200fcc819e
motokishibata <shibata.youkan0701@gmail.com>
2018-01-20 07:19:16 +0000

    posts function routing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,45 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @like = Like.new()
   end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = current_user.posts.build(posts_params)
+    
+    if @post.save
+      redirect_to :action => "index"
+    else
+      render :action => "new"
+    end
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update(posts_params)
+      redirect_to :action => "index"
+    else
+      render :action => "edit"
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to posts_url
+  end
+  
+  private
+  
+  def posts_params
+    params.require(:post).permit(:title, :body, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'toppages/index'
-
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+  
+  resources :posts
   
   resources :users, only: [:index, :show] do
     resources :posts
